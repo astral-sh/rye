36ce5f8d10b0ce645bfa3429a30a23a8b7e530e4
Armin Ronacher <armin.ronacher@active-4.com>
2023-04-27 09:22:49 +0200

    Added a more obvious way to install from git and HTTP
    Refs #53

--- rye/src/cli/install.rs
+++ rye/src/cli/install.rs
@@ -1,4 +1,4 @@
-use anyhow::Error;
+use anyhow::{bail, Context, Error};
 use clap::Parser;
 use pep508_rs::Requirement;
 
@@ -9,8 +9,28 @@ use crate::utils::CommandOutput;
 /// Installs a package as global tool.
 #[derive(Parser, Debug)]
 pub struct Args {
-    /// The package to install.
+    /// The name of the package to install.
     requirement: String,
+    /// Install the given package from this git repository
+    #[arg(long)]
+    git: Option<String>,
+    /// Install the given package from this URL
+    #[arg(long, conflicts_with = "git")]
+    url: Option<String>,
+    /// Install a specific tag.
+    #[arg(long, requires = "git")]
+    tag: Option<String>,
+    /// Update to a specific git rev.
+    #[arg(
+        long,
+        conflicts_with = "tag",
+        conflicts_with = "branch",
+        requires = "git"
+    )]
+    rev: Option<String>,
+    /// Update to a specific git branch.
+    #[arg(long, conflicts_with = "tag", conflicts_with = "rev", requires = "git")]
+    branch: Option<String>,
     /// Optionally the Python version to use.
     #[arg(short, long)]
     python: Option<String>,
@@ -27,6 +47,48 @@ pub struct Args {
 
 pub fn execute(cmd: Args) -> Result<(), Error> {
     let output = CommandOutput::from_quiet_and_verbose(cmd.quiet, cmd.verbose);
+
+    let mut requirement: Requirement = cmd
+        .requirement
+        .parse()
+        .with_context(|| {
+            if cmd.requirement.contains("://") {
+                format!("failed to parse requirement '{}'. It looks like a URL, maybe you wanted to use --url or --git", cmd.requirement)
+            } else {
+                format!("failed to parse requirement '{}'", cmd.requirement)
+            }
+        })?;
+    if let Some(ref git) = cmd.git {
+        // XXX: today they are all aliases, it might be better to change
+        // tag to refs/tags/<tag> and branch to refs/heads/<branch> but
+        // this creates some ugly warnings in pip today
+        let suffix = match cmd
+            .rev
+            .as_ref()
+            .or(cmd.tag.as_ref())
+            .or(cmd.branch.as_ref())
+        {
+            Some(rev) => format!("@{}", rev),
+            None => "".into(),
+        };
+        requirement.version_or_url = match requirement.version_or_url {
+            Some(_) => bail!("requirement already has a version marker"),
+            None => Some(pep508_rs::VersionOrUrl::Url(
+                format!("git+{}{}", git, suffix).parse().with_context(|| {
+                    format!("unable to interpret '{}{}' as git reference", git, suffix)
+                })?,
+            )),
+        };
+    } else if let Some(ref url) = cmd.url {
+        requirement.version_or_url = match requirement.version_or_url {
+            Some(_) => bail!("requirement already has a version marker"),
+            None => Some(pep508_rs::VersionOrUrl::Url(
+                url.parse()
+                    .with_context(|| format!("unable to parse '{}' as url", url))?,
+            )),
+        };
+    }
+
     let py_ver: PythonVersionRequest = match cmd.python {
         Some(ref py) => py.parse()?,
         None => PythonVersionRequest {
@@ -37,7 +99,6 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
             suffix: None,
         },
     };
-    let requirement: Requirement = cmd.requirement.parse()?;
 
     install(requirement, &py_ver, cmd.force, output)?;
     Ok(())
