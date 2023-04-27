2286d76cfc641e7ff7fb4fbc42d1dd3ca063888b
Armin Ronacher <armin.ronacher@active-4.com>
2023-04-27 09:35:41 +0200

    Share options between install and add

--- rye/src/cli/install.rs
+++ rye/src/cli/install.rs
@@ -1,7 +1,8 @@
-use anyhow::{bail, Context, Error};
+use anyhow::{Context, Error};
 use clap::Parser;
 use pep508_rs::Requirement;
 
+use crate::cli::add::ReqExtras;
 use crate::installer::install;
 use crate::sources::PythonVersionRequest;
 use crate::utils::CommandOutput;
@@ -11,26 +12,8 @@ use crate::utils::CommandOutput;
 pub struct Args {
     /// The name of the package to install.
     requirement: String,
-    /// Install the given package from this git repository
-    #[arg(long)]
-    git: Option<String>,
-    /// Install the given package from this URL
-    #[arg(long, conflicts_with = "git")]
-    url: Option<String>,
-    /// Install a specific tag.
-    #[arg(long, requires = "git")]
-    tag: Option<String>,
-    /// Update to a specific git rev.
-    #[arg(
-        long,
-        conflicts_with = "tag",
-        conflicts_with = "branch",
-        requires = "git"
-    )]
-    rev: Option<String>,
-    /// Update to a specific git branch.
-    #[arg(long, conflicts_with = "tag", conflicts_with = "rev", requires = "git")]
-    branch: Option<String>,
+    #[command(flatten)]
+    req_extras: ReqExtras,
     /// Optionally the Python version to use.
     #[arg(short, long)]
     python: Option<String>,
@@ -58,36 +41,7 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
                 format!("failed to parse requirement '{}'", cmd.requirement)
             }
         })?;
-    if let Some(ref git) = cmd.git {
-        // XXX: today they are all aliases, it might be better to change
-        // tag to refs/tags/<tag> and branch to refs/heads/<branch> but
-        // this creates some ugly warnings in pip today
-        let suffix = match cmd
-            .rev
-            .as_ref()
-            .or(cmd.tag.as_ref())
-            .or(cmd.branch.as_ref())
-        {
-            Some(rev) => format!("@{}", rev),
-            None => "".into(),
-        };
-        requirement.version_or_url = match requirement.version_or_url {
-            Some(_) => bail!("requirement already has a version marker"),
-            None => Some(pep508_rs::VersionOrUrl::Url(
-                format!("git+{}{}", git, suffix).parse().with_context(|| {
-                    format!("unable to interpret '{}{}' as git reference", git, suffix)
-                })?,
-            )),
-        };
-    } else if let Some(ref url) = cmd.url {
-        requirement.version_or_url = match requirement.version_or_url {
-            Some(_) => bail!("requirement already has a version marker"),
-            None => Some(pep508_rs::VersionOrUrl::Url(
-                url.parse()
-                    .with_context(|| format!("unable to parse '{}' as url", url))?,
-            )),
-        };
-    }
+    cmd.req_extras.apply_to_requirement(&mut requirement)?;
 
     let py_ver: PythonVersionRequest = match cmd.python {
         Some(ref py) => py.parse()?,
