0d935b9983b4a6734cb20d541d97ca78c838e0a5
Armin Ronacher <armin.ronacher@active-4.com>
2023-05-17 01:44:33 +0200

    Add Functioning Update Command (#176)

--- rye/src/cli/rye.rs
+++ rye/src/cli/rye.rs
@@ -10,10 +10,16 @@ use console::style;
 use minijinja::render;
 use same_file::is_same_file;
 
-use crate::bootstrap::ensure_self_venv;
+use crate::bootstrap::{download_url, ensure_self_venv};
 use crate::platform::get_app_dir;
 use crate::utils::{CommandOutput, QuietExit};
 
+#[cfg(windows)]
+const DEFAULT_HOME: &str = "%USERPROFILE%\\.rye";
+#[cfg(unix)]
+const DEFAULT_HOME: &str = "$HOME/.rye";
+
+const GITHUB_REPO: &str = "https://github.com/mitsuhiko/rye";
 const UNIX_ENV_FILE: &str = r#"
 # rye shell setup
 {%- if custom_home %}
@@ -50,6 +56,9 @@ pub struct CompletionCommand {
 /// right arguments.
 #[derive(Parser, Debug)]
 pub struct UpdateCommand {
+    /// Update to a specific version.
+    #[arg(long)]
+    version: Option<String>,
     /// Update to a specific tag.
     #[arg(long)]
     tag: Option<String>,
@@ -97,27 +106,62 @@ fn completion(args: CompletionCommand) -> Result<(), Error> {
 }
 
 fn update(args: UpdateCommand) -> Result<(), Error> {
-    let mut helper = rename_helper::RenameHelper::new()?;
-    let mut cmd = Command::new("cargo");
-    cmd.arg("install")
-        .arg("--git")
-        .arg("https://github.com/mitsuhiko/rye");
-    if let Some(ref rev) = args.rev {
-        cmd.arg("--rev");
-        cmd.arg(rev);
-    } else if let Some(ref tag) = args.tag {
-        cmd.arg("--tag");
-        cmd.arg(tag);
+    // git based installation with cargo
+    if args.rev.is_some() || args.tag.is_some() {
+        let mut cmd = Command::new("cargo");
+        cmd.arg("install")
+            .arg("--git")
+            .arg("https://github.com/mitsuhiko/rye");
+        if let Some(ref rev) = args.rev {
+            cmd.arg("--rev");
+            cmd.arg(rev);
+        } else if let Some(ref tag) = args.tag {
+            cmd.arg("--tag");
+            cmd.arg(tag);
+        }
+        if args.force {
+            cmd.arg("--force");
+        }
+        cmd.arg("rye");
+        let status = cmd.status().context("unable to update via cargo-install")?;
+        if !status.success() {
+            bail!("failed to self-update via cargo-install");
+        }
+    } else {
+        let version = args.version.as_deref().unwrap_or("latest");
+        eprintln!("Updating to {version}");
+        let binary = format!("rye-{ARCH}-{OS}");
+        let ext = if cfg!(unix) { ".gz" } else { ".exe" };
+        let url = if version == "latest" {
+            format!("{GITHUB_REPO}/releases/latest/download/{binary}{ext}")
+        } else {
+            format!("{GITHUB_REPO}/releases/download/{version}/{binary}{ext}")
+        };
+        let bytes = download_url(&url, CommandOutput::Normal)
+            .with_context(|| format!("could not download {version} release for this platform"))?;
+        let tmp = tempfile::NamedTempFile::new()?;
+
+        // unix currently comes compressed, windows comes uncompressed
+        #[cfg(unix)]
+        {
+            use std::io::Read;
+            let mut decoder = flate2::bufread::GzDecoder::new(&bytes[..]);
+            let mut rv = Vec::new();
+            decoder.read_to_end(&mut rv)?;
+            fs::write(tmp.path(), rv)?;
+        }
+        #[cfg(windows)]
+        {
+            fs::write(tmp.path(), bytes)?;
+        }
+
+        self_replace::self_replace(tmp.path())?;
+        eprintln!("Updated!");
+        eprintln!();
+        Command::new(env::current_exe()?)
+            .arg("--version")
+            .status()?;
     }
-    if args.force {
-        cmd.arg("--force");
-    }
-    cmd.arg("rye");
-    let status = cmd.status().context("unable to update via cargo-install")?;
-    if !status.success() {
-        bail!("failed to self-update via cargo-install");
-    }
-    helper.disarm();
 
     Ok(())
 }
@@ -126,7 +170,7 @@ fn install(_args: InstallCommand) -> Result<(), Error> {
     let exe = env::current_exe()?;
     let app_dir = get_app_dir();
     let shims = app_dir.join("shims");
-    let target = shims.join(format!("rye{EXE_EXTENSION}"));
+    let target = shims.join("rye").with_extension(EXE_EXTENSION);
 
     eprintln!("{}", style("Welcome to Rye!").bold());
     eprintln!();
@@ -158,17 +202,20 @@ fn install(_args: InstallCommand) -> Result<(), Error> {
         fs::remove_file(&target)?;
     }
     fs::copy(exe, &target)?;
+    eprintln!("Installed binary to {}", style(target.display()).cyan());
 
     // write an env file we can source later.  Prefer $HOME/.rye over
     // the expanded path, if not overridden.
     let (custom_home, rye_home) = env::var("RYE_HOME")
         .map(|x| (true, Cow::Owned(x)))
-        .unwrap_or((false, Cow::Borrowed("$HOME/.rye")));
-    fs::write(
-        app_dir.join("env"),
-        render!(UNIX_ENV_FILE, custom_home, rye_home),
-    )?;
-    eprintln!("Installed binary to {}", style(target.display()).cyan());
+        .unwrap_or((false, Cow::Borrowed(DEFAULT_HOME)));
+
+    if cfg!(unix) {
+        fs::write(
+            app_dir.join("env"),
+            render!(UNIX_ENV_FILE, custom_home, rye_home),
+        )?;
+    }
 
     // Ensure internals next
     let self_path = ensure_self_venv(CommandOutput::Normal)?;
@@ -177,76 +224,32 @@ fn install(_args: InstallCommand) -> Result<(), Error> {
         style(self_path.display()).cyan()
     );
 
-    if !env::split_paths(&env::var_os("PATH").unwrap())
-        .any(|x| is_same_file(x, &shims).unwrap_or(false))
-    {
+    if cfg!(unix) {
+        if !env::split_paths(&env::var_os("PATH").unwrap())
+            .any(|x| is_same_file(x, &shims).unwrap_or(false))
+        {
+            eprintln!();
+            eprintln!(
+                "The rye directory {} was not detected on {}.",
+                style(shims.display()).cyan(),
+                style("PATH").cyan()
+            );
+            eprintln!("It is highly recommended that you add it.");
+            eprintln!("Add this at the end of your .profile, .zprofile or similar:");
+            eprintln!();
+            eprintln!("    source \"{}/env\"", rye_home);
+            eprintln!();
+            eprintln!(
+                "Note: after adding rye to your path, restart your shell for it to take effect."
+            );
+        }
+    } else if cfg!(windows) {
         eprintln!();
-        eprintln!(
-            "The rye directory {} was not detected on {}.",
-            style(shims.display()).cyan(),
-            style("PATH").cyan()
-        );
-        eprintln!("It is highly recommended that you add it.");
-        eprintln!("Add this at the end of your .profile, .zprofile or similar:");
-        eprintln!();
-        eprintln!("    source \"{}/env\"", rye_home);
-        eprintln!();
-        eprintln!("Note: after adding rye to your path, restart your shell for it to take effect.");
+        eprintln!("Note: You need to manually add {DEFAULT_HOME} to your PATH.");
     }
 
     eprintln!();
     eprintln!("{}", style("All done!").green());
 
     Ok(())
-}
-
-#[cfg(windows)]
-mod rename_helper {
-    use super::*;
-    use std::{env, fs, path::PathBuf};
-
-    pub struct RenameHelper {
-        original_path: PathBuf,
-        path: PathBuf,
-        disarmed: bool,
-    }
-
-    impl RenameHelper {
-        pub fn new() -> Result<RenameHelper, Error> {
-            let original_path = env::current_exe()?;
-            let path = original_path.with_extension("tmp");
-            fs::rename(&original_path, &path)?;
-            Ok(RenameHelper {
-                original_path,
-                path,
-                disarmed: false,
-            })
-        }
-
-        pub fn disarm(&mut self) {
-            self.disarmed = true;
-        }
-    }
-
-    impl Drop for RenameHelper {
-        fn drop(&mut self) {
-            if !self.disarmed {
-                fs::rename(&self.path, &self.original_path).ok();
-            }
-        }
-    }
-}
-
-#[cfg(unix)]
-mod rename_helper {
-    use super::*;
-    pub struct RenameHelper;
-
-    impl RenameHelper {
-        pub fn new() -> Result<RenameHelper, Error> {
-            Ok(RenameHelper)
-        }
-
-        pub fn disarm(&mut self) {}
-    }
 }
