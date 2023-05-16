c9bd9f4e558d8a6844a45870374430aad839a23b
Armin Ronacher <armin.ronacher@active-4.com>
2023-05-17 00:46:06 +0200

    Use gzip compression on installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,9 +27,9 @@ BINARY="rye-${ARCH}-${PLATFORM}"
 
 # Oddly enough GitHub has different URLs for latest vs specific version
 if [[ $VERSION == "latest" ]]; then
-  DOWNLOAD_URL=https://github.com/${REPO}/releases/latest/download/${BINARY}
+  DOWNLOAD_URL=https://github.com/${REPO}/releases/latest/download/${BINARY}.gz
 else
-  DOWNLOAD_URL=https://github.com/${REPO}/releases/download/${VERSION}/${BINARY}
+  DOWNLOAD_URL=https://github.com/${REPO}/releases/download/${VERSION}/${BINARY}.gz
 fi
 
 echo "This script will automatically download and install rye (${VERSION}) for you."
@@ -42,18 +42,26 @@ if ! hash curl 2> /dev/null; then
   exit 1
 fi
 
+if ! hash gunzip 2> /dev/null; then
+  echo "error: you do not have 'gunzip' installed which is required for this script."
+  exit 1
+fi
+
 TEMP_FILE=`mktemp "${TMPDIR:-/tmp}/.ryeinstall.XXXXXXXX"`
+TEMP_FILE_GZ="${TEMP_FILE}.gz"
 
 cleanup() {
   rm -f "$TEMP_FILE"
+  rm -f "$TEMP_FILE_GZ"
 }
 
 trap cleanup EXIT
-HTTP_CODE=$(curl -SL --progress-bar "$DOWNLOAD_URL" --output "$TEMP_FILE" --write-out "%{http_code}")
+HTTP_CODE=$(curl -SL --progress-bar "$DOWNLOAD_URL" --output "$TEMP_FILE_GZ" --write-out "%{http_code}")
 if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]]; then
   echo "error: your platform and architecture (${PLATFORM}-${ARCH}) is unsupported."
   exit 1
 fi
 
+gunzip "$TEMP_FILE_GZ"
 chmod +x "$TEMP_FILE"
 "$TEMP_FILE" self install
