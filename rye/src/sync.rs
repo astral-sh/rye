97bb44ea4e4a64ae6d581de85bfd01cda610e19d
Armin Ronacher <armin.ronacher@active-4.com>
2023-05-29 00:49:38 +0200

    Make tcl init logic slightly more resilient

--- rye/src/sync.rs
+++ rye/src/sync.rs
@@ -292,6 +292,9 @@ pub fn create_virtualenv(
         bail!("failed to initialize virtualenv");
     }
 
+    // On UNIX systems Python is unable to find the tcl config that is placed
+    // outside of the virtualenv.  It also sometimes is entirely unable to find
+    // the tcl config that comes from the standalone python builds.
     #[cfg(unix)]
     {
         inject_tcl_config(venv, &py_bin, py_ver)?;
@@ -300,9 +303,6 @@ pub fn create_virtualenv(
     Ok(())
 }
 
-/// On UNIX systems Python is unable to find the tcl config that is placed outside of the
-/// virtualenv.  It also sometimes is entirely unable to find the tcl config that comes
-/// from the standalone python builds.
 #[cfg(unix)]
 fn inject_tcl_config(venv: &Path, py_bin: &Path, py_ver: &PythonVersion) -> Result<(), Error> {
     let lib_path = match py_bin
@@ -314,32 +314,53 @@ fn inject_tcl_config(venv: &Path, py_bin: &Path, py_ver: &PythonVersion) -> Resu
         None => return Ok(()),
     };
 
-    let tcl_lib = lib_path.join("tcl8.6");
-    let tcl_lib_exists = tcl_lib.is_dir();
-    let tk_lib = lib_path.join("tk8.6");
-    let tk_lib_exists = tk_lib.is_dir();
+    let mut tcl_lib = None;
+    let mut tk_lib = None;
+
+    if let Ok(dir) = lib_path.read_dir() {
+        for entry in dir.filter_map(|x| x.ok()) {
+            let filename = entry.file_name();
+            let name = match filename.to_str() {
+                Some(name) => name,
+                None => continue,
+            };
+            if name.starts_with("tcl8") {
+                tcl_lib = Some(name.to_string());
+                if tk_lib.is_some() {
+                    break;
+                }
+            } else if name.starts_with("tk8") {
+                tk_lib = Some(name.to_string());
+                if tcl_lib.is_some() {
+                    break;
+                }
+            }
+        }
+    }
+
     let site_packages = venv
         .join("lib")
         .join(format!("python{}.{}", py_ver.major, py_ver.minor))
         .join("site-packages");
 
-    if !(tk_lib_exists || tcl_lib_exists) {
+    if tk_lib.is_none() && tcl_lib.is_none() {
         return Ok(());
     }
 
-    let pth = minijinja::render!(
-        r#"import os, sys;
-{%- if tcl_lib_exists -%}
-os.environ.setdefault('TCL_LIBRARY', sys.base_prefix + '/lib/tcl8.6');
+    fs::write(
+        site_packages.join("_tcl-init.pth"),
+        minijinja::render!(
+            r#"import os, sys;
+{%- if tcl_lib -%}
+os.environ.setdefault('TCL_LIBRARY', sys.base_prefix + '/lib/{{ tcl_lib }}');
 {%- endif -%}
-{%- if tk_lib_exists -%}
-os.environ.setdefault('TK_LIBRARY', sys.base_prefix + '/lib/tk8.6');
+{%- if tk_lib -%}
+os.environ.setdefault('TK_LIBRARY', sys.base_prefix + '/lib/{{ tk_lib }}');
 {%- endif -%}"#,
-        tcl_lib_exists,
-        tk_lib_exists,
-    );
-
-    fs::write(site_packages.join("_tcl-init.pth"), pth)?;
+            tcl_lib,
+            tk_lib,
+        ),
+    )?;
 
     Ok(())
 }
