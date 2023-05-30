38f82abf48153516d81d6302cced7d2921c85ce1
Chris Pryer <cnpryer@gmail.com>
2023-05-30 13:44:14 -0400

    First-pass init --import w/o requirement file parsing

--- rye/src/cli/init.rs
+++ rye/src/cli/init.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::str::FromStr;
 use std::{env, fs};
@@ -6,12 +6,18 @@ use std::{env, fs};
 use anyhow::{anyhow, bail, Context, Error};
 use clap::Parser;
 use console::style;
+use ini::Ini;
 use license::License;
 use minijinja::{context, Environment};
 use pep440_rs::VersionSpecifier;
+use serde_json::Value;
+use tempfile::tempdir;
 
 use crate::config::Config;
-use crate::platform::{get_default_author, get_latest_cpython, get_python_version_from_pyenv_pin};
+use crate::platform::{
+    get_default_author, get_latest_cpython, get_python_version_from_pyenv_pin,
+    get_toolchain_python_bin,
+};
 use crate::pyproject::BuildSystem;
 use crate::sources::PythonVersion;
 use crate::utils::is_inside_git_work_tree;
@@ -43,6 +49,15 @@ pub struct Args {
     /// The name of the package.
     #[arg(long)]
     name: Option<String>,
+    /// Import a project with a setup.cfg, setup.py, or requirements files.
+    #[arg(long)]
+    import: bool,
+    /// Requirements files to initialize pyproject.toml with.
+    #[arg(short, long, name = "requirements_file")]
+    requirements: Option<Vec<PathBuf>>,
+    /// Development requirements files to initialize pyproject.toml with.
+    #[arg(long, name = "dev_requirements_file")]
+    dev_requirements: Option<Vec<PathBuf>>,
 }
 
 /// The pyproject.toml template
@@ -51,7 +66,7 @@ pub struct Args {
 const TOML_TEMPLATE: &str = r#"[project]
 name = {{ name }}
 version = {{ version }}
-description = "Add a short description here"
+description = {{ description }}
 {%- if author %}
 authors = [
     { name = {{ author[0] }}, email = {{ author[1] }} }
@@ -127,6 +142,27 @@ wheels/
 
 "#;
 
+/// Script used for setup.py setup proxy.
+const SETUP_PY_PROXY_SCRIPT: &str = r#"
+import json, os, shutil, subprocess, sys
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+def setup(**kwargs) -> None:
+    print(json.dumps(kwargs), file=sys.stderr)
+
+if __name__ == "setuptools":
+    _setup_proxy_module = sys.modules.pop("setuptools")
+    _setup_proxy_cwd = sys.path.pop(0)
+    import setuptools as __setuptools
+    sys.path.insert(0, _setup_proxy_cwd)
+    sys.modules["setuptools"] = _setup_proxy_module
+    def __getattr__(name):
+        return getattr(__setuptools, name)
+    del _setup_proxy_module
+    del _setup_proxy_cwd
+"#;
+
 pub fn execute(cmd: Args) -> Result<(), Error> {
     let cfg = Config::current();
     let env = Environment::new();
@@ -197,6 +233,22 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
         fs::write(&license_file, rv)?;
     }
 
+    let mut metadata = Metadata {
+        name,
+        version: version.to_string(),
+        description: "Add your description here".to_string(),
+        author,
+        requires_python: Some(requires_python),
+        license,
+        dependencies: None,
+        dev_dependencies: None,
+    };
+    if cmd.import {
+        // TODO(cnpryer): May need to be smarter with what Python version is used
+        let python = get_toolchain_python_bin(&get_latest_cpython()?)?;
+        import_project_metadata(&mut metadata, &dir, &python, None, None)?;
+    }
+
     // write .python-version
     if !cmd.no_pin && !python_version_file.is_file() {
         fs::write(python_version_file, format!("{}\n", py))
@@ -211,8 +263,8 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
             "README.txt",
             README_TEMPLATE,
             context! {
-                name,
-                license,
+                name => metadata.name,
+                license => metadata.license,
             },
         )?;
         fs::write(&readme, rv)?;
@@ -230,11 +282,12 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
         "pyproject.json",
         TOML_TEMPLATE,
         context! {
-            name,
-            version,
-            author,
-            requires_python,
-            license,
+            name => metadata.name,
+            description => metadata.description,
+            version => metadata.version,
+            author => metadata.author,
+            requires_python => metadata.requires_python,
+            license => metadata.license,
             with_readme,
             build_system,
         },
@@ -243,9 +296,13 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
 
     let src_dir = dir.join("src");
     if !src_dir.is_dir() {
-        let project_dir = src_dir.join(name.replace('-', "_"));
+        let project_dir = src_dir.join(metadata.name.replace('-', "_"));
         fs::create_dir_all(&project_dir).ok();
-        let rv = env.render_named_str("__init__.py", INIT_PY_TEMPLATE, context! { name })?;
+        let rv = env.render_named_str(
+            "__init__.py",
+            INIT_PY_TEMPLATE,
+            context! { name => metadata.name },
+        )?;
         fs::write(project_dir.join("__init__.py"), rv).context("failed to write __init__.py")?;
     }
 
@@ -275,6 +332,182 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
         dir.display()
     );
     eprintln!("  Run `rye sync` to get started");
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct Metadata {
+    name: String,
+    version: String,
+    description: String,
+    author: Option<(String, String)>,
+    requires_python: Option<String>,
+    license: Option<String>,
+    dependencies: Option<Vec<String>>,
+    dev_dependencies: Option<Vec<String>>,
+}
+
+fn import_project_metadata<T: AsRef<Path>>(
+    metadata: &mut Metadata,
+    dir: T,
+    python: T,
+    requirements_files: Option<Vec<PathBuf>>,
+    dev_requirements_files: Option<Vec<PathBuf>>,
+) -> Result<&mut Metadata, Error> {
+    let dir = dir.as_ref();
+    let python = python.as_ref();
+    let setup_cfg = dir.join("setup.cfg");
+    let setup_py = dir.join("setup.py");
+
+    // TODO(cnpryer): Start with setup.py import and then selectively import from cfg
+    if let Ok(ini) = Ini::load_from_file(setup_cfg) {
+        if let Some(section) = ini.section(Some("metadata")) {
+            if let Some(name) = section.get("name") {
+                metadata.name = name.to_string();
+            }
+            if let Some(version) = section.get("version") {
+                metadata.version = version.to_string();
+            }
+            if let Some(description) = section.get("description") {
+                metadata.description = description.to_string();
+            }
+            if let Some(it) = section.get("author") {
+                metadata.author = Some((
+                    it.to_string(),
+                    section.get("author_email").unwrap_or("").to_string(),
+                ));
+            }
+            metadata.license = section.get("license").map(|x| x.to_string());
+        }
+        if let Some(section) = ini.section(Some("options")) {
+            metadata.requires_python = section.get("requires_python").map(|x| x.to_string());
+            if let Some(reqs) = section.get("install_requires") {
+                metadata.dependencies = Some(reqs.lines().map(|x| x.to_string()).collect());
+            }
+        }
+    }
+
+    if let Ok(json) = get_setup_py_json(setup_py.as_path(), python) {
+        if let Some(name) = json.get("name") {
+            metadata.name = name.to_string();
+        }
+        if let Some(version) = json.get("version") {
+            metadata.version = version.to_string();
+        }
+        if let Some(description) = json.get("description") {
+            metadata.description = description.to_string();
+        }
+        if let Some(it) = json.get("author") {
+            metadata.author = Some((
+                it.to_string(),
+                json.get("author_email")
+                    .map(|x| x.to_string())
+                    .unwrap_or_else(String::new),
+            ));
+        }
+        if let Some(requires_python) = json.get("requires_python") {
+            metadata.requires_python = Some(requires_python.to_string());
+        }
+        if let Some(license) = json.get("license") {
+            metadata.license = Some(license.to_string());
+        }
+        if let Some(Value::Array(reqs)) = json.get("install_requires") {
+            metadata.dependencies = Some(
+                reqs.iter()
+                    .filter_map(|value| value.as_str().map(String::from))
+                    .collect::<Vec<String>>(),
+            );
+        }
+    }
+
+    if let Some(paths) = requirements_files {
+        for p in paths {
+            if let Ok(deps) = parse_requirements_file(p) {
+                if let Some(x) = metadata.dependencies.as_mut() {
+                    x.extend(deps)
+                }
+            }
+        }
+    }
+    if let Some(paths) = dev_requirements_files {
+        for p in paths {
+            if let Ok(deps) = parse_requirements_file(p) {
+                if let Some(x) = metadata.dev_dependencies.as_mut() {
+                    x.extend(deps)
+                }
+            }
+        }
+    }
+    if let Some(x) = metadata.dependencies.as_mut() {
+        x.dedup()
+    }
+    if let Some(x) = metadata.dev_dependencies.as_mut() {
+        x.dedup()
+    }
+
+    Ok(metadata)
+}
+
+fn get_setup_py_json<T: AsRef<Path>>(path: T, python: T) -> Result<Value, Error> {
+    let python = python.as_ref();
+    let setup_py = path.as_ref();
+    let temp_dir = tempdir().unwrap();
+    let dir = setup_py
+        .parent()
+        .context("could not establish setup.py parent dir")?;
+    copy_dir(dir, temp_dir.path()).unwrap();
+
+    let setuptools_proxy = temp_dir.path().join("setuptools.py");
+    fs::write(setuptools_proxy, SETUP_PY_PROXY_SCRIPT).unwrap();
+
+    let cmd = Command::new(python)
+        .arg(setup_py)
+        .env("PYTHONPATH", temp_dir.path())
+        .stderr(Stdio::piped())
+        .output()?;
+    if cmd.status.success() {
+        Ok(serde_json::from_slice(&cmd.stderr)?)
+    } else {
+        let log = String::from_utf8_lossy(&cmd.stderr);
+        bail!("failed to proxy setup.py\n{}", log);
+    }
+}
+
+fn parse_requirements_file<T: AsRef<Path>>(_path: T) -> Result<Vec<String>, Error> {
+    todo!()
+}
+
+// TODO(cnpryer)
+pub fn copy_dir<T: AsRef<Path>>(from: T, to: T) -> Result<(), Error> {
+    let (from, to) = (from.as_ref(), to.as_ref());
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from));
+    let target_root = to.to_path_buf();
+    let from_component_count = from.to_path_buf().components().count();
+    while let Some(working_path) = stack.pop() {
+        // Collects the trailing components of the path
+        let src: PathBuf = working_path
+            .components()
+            .skip(from_component_count)
+            .collect();
+        let dest = if src.components().count() == 0 {
+            target_root.clone()
+        } else {
+            target_root.join(&src)
+        };
+        if !dest.exists() {
+            fs::create_dir_all(&dest).expect("to create dir");
+        }
+        for entry in fs::read_dir(working_path).expect("to read dir") {
+            let path = entry.expect("an entry").path();
+            if path.is_dir() {
+                stack.push(path);
+            } else if let Some(filename) = path.file_name() {
+                fs::copy(&path, dest.join(filename)).expect("to copy");
+            }
+        }
+    }
 
     Ok(())
 }
