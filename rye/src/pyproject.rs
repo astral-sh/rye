a33c8be8e68e6aca18fb8f484d5f672ad1a6d063
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2023-06-01 21:46:09 +0200

    pyproject: Emit specific error for not discovering

--- rye/src/pyproject.rs
+++ rye/src/pyproject.rs
@@ -445,6 +445,18 @@ fn skip_recurse_into(name: &OsStr) -> bool {
     return name == OsStr::new(".venv") || name == OsStr::new(".git");
 }
 
+/// Could not auto-discover any pyproject
+#[derive(Debug, Clone)]
+pub struct DiscoveryUnsuccessful;
+
+impl std::error::Error for DiscoveryUnsuccessful {}
+
+impl fmt::Display for DiscoveryUnsuccessful {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "did not find pyproject.toml")
+    }
+}
+
 /// Helps working with pyproject.toml files
 #[derive(Debug)]
 pub struct PyProject {
@@ -458,7 +470,7 @@ impl PyProject {
     pub fn discover() -> Result<PyProject, Error> {
         let pyproject_toml = match find_project_root() {
             Some(root) => root.join("pyproject.toml"),
-            None => bail!("did not find pyproject.toml"),
+            None => return Err(Error::from(DiscoveryUnsuccessful)),
         };
         Self::load(&pyproject_toml)
     }
