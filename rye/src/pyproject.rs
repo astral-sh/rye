562fd61019fb1880a4bfa674234b73b7bdc15a00
CharlesChen0823 <yongchen0823@gmail.com>
2023-06-02 10:21:06 +0800

    fixed pin

--- rye/src/pyproject.rs
+++ rye/src/pyproject.rs
@@ -24,7 +24,7 @@ use url::Url;
 use crate::config::Config;
 use crate::consts::VENV_BIN;
 use crate::platform::{get_python_version_request_from_pyenv_pin, list_known_toolchains};
-use crate::sources::{get_download_url, PythonVersion, PythonVersionRequest};
+use crate::sources::{get_download_url, PythonVersion, PythonVersionRequest, matches_version};
 use crate::sync::VenvMarker;
 use crate::utils::{
     expand_env_vars, format_requirement, get_short_executable_name, is_executable,
@@ -892,7 +892,7 @@ pub fn latest_available_python_version(
         available
             .into_iter()
             .filter_map(|(ver, _)| {
-                if Some(&ver.kind as &str) == requested_version.kind.as_deref() {
+                if matches_version(requested_version, &ver) {
                     Some(ver)
                 } else {
                     None
