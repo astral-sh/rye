3088a7ed1e536385d8114f4b9031ae715468804f
Chaojie <hi@chaojie.fun>
2023-06-05 18:34:44 +0800

    Support bump version with dev and post

--- rye/src/cli/version.rs
+++ rye/src/cli/version.rs
@@ -1,17 +1,17 @@
 use std::str::FromStr;
 
+use crate::pyproject::PyProject;
 use anyhow::{anyhow, Error};
 use clap::Parser;
+use console::style;
 use pep440_rs::Version;
-
-use crate::pyproject::PyProject;
 
 /// Get or set project version
 #[derive(Parser, Debug)]
 pub struct Args {
     /// The version to set
     version: Option<String>,
-    /// The version bump to apply
+    /// The version bump to apply [major, minor, patch]
     #[arg(short, long)]
     bump: Option<String>,
 }
@@ -29,27 +29,36 @@ pub fn execute(cmd: Args) -> Result<(), Error> {
         }
         None => {
             let mut version = pyproject_toml.version()?;
-            if let Some(bump) = cmd.bump {
-                let bumped = bump_version(&mut version, &bump);
-                pyproject_toml.set_version(&version);
-                pyproject_toml.save()?;
-                if bumped {
-                    eprintln!("version bumped to {}", version);
-                }
-            } else {
-                eprintln!("{}", version);
+            match cmd.bump {
+                Some(bump) => bump_version(&mut version, &bump, &mut pyproject_toml),
+                None => eprintln!("{}", version),
             }
         }
     }
     Ok(())
 }
 
-fn bump_version(version: &mut Version, bump: &str) -> bool {
-    match bump {
-        "major" => version.release[0] += 1,
-        "minor" => version.release[1] += 1,
-        "patch" => version.release[2] += 1,
-        _ => return false,
+fn bump_version(version: &mut Version, bump: &str, pyproject: &mut PyProject) {
+    if version.is_post() {
+        version.post = None;
     }
-    true
+    if version.is_dev() {
+        version.dev = None;
+        eprintln!(
+            "{} dev version will be bumped to release version",
+            style("warning:").red()
+        );
+    } else {
+        match bump {
+            "major" => version.release[0] += 1,
+            "minor" => version.release[1] += 1,
+            "patch" => version.release[2] += 1,
+            _ => return,
+        }
+    }
+
+    pyproject.set_version(&version);
+    pyproject.save().unwrap();
+
+    eprintln!("version bumped to {}", version);
 }
