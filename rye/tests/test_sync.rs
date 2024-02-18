435884be4de6adcfdfba99874ed217320229e6ff
Armin Ronacher <armin.ronacher@active-4.com>
2024-02-19 00:02:57 +0100

    Force colorama

--- rye/tests/test_sync.rs
+++ rye/tests/test_sync.rs
@@ -34,12 +34,14 @@ fn test_empty_sync() {
 fn test_add_and_sync() {
     let space = Space::new();
     space.init("my-project");
-    rye_cmd_snapshot!(space.rye_cmd().arg("add").arg("flask==3.0.0"), @r###"
+    // add colorama to ensure we have this as a dependency on all platforms
+    rye_cmd_snapshot!(space.rye_cmd().arg("add").arg("flask==3.0.0").arg("colorama"), @r###"
     success: true
     exit_code: 0
     ----- stdout -----
     Initializing new virtualenv in [TEMP_PATH]/project/.venv
     Python version: cpython@3.12.1
+    Added colorama>=0.4.6 as regular dependency
     Added flask>=3.0.0 as regular dependency
 
     ----- stderr -----
@@ -57,16 +59,17 @@ fn test_add_and_sync() {
     ----- stderr -----
     warning: Requirements file [TEMP_FILE] does not contain any dependencies
     Built 1 editable in [EXECUTION_TIME]
-    Resolved 8 packages in [EXECUTION_TIME]
+    Resolved 9 packages in [EXECUTION_TIME]
     warning: Requirements file [TEMP_FILE] does not contain any dependencies
     Built 1 editable in [EXECUTION_TIME]
-    Resolved 8 packages in [EXECUTION_TIME]
+    Resolved 9 packages in [EXECUTION_TIME]
     Built 1 editable in [EXECUTION_TIME]
-    Resolved 7 packages in [EXECUTION_TIME]
-    Downloaded 7 packages in [EXECUTION_TIME]
-    Installed 8 packages in [EXECUTION_TIME]
+    Resolved 8 packages in [EXECUTION_TIME]
+    Downloaded 8 packages in [EXECUTION_TIME]
+    Installed 9 packages in [EXECUTION_TIME]
      + blinker==1.7.0
      + click==8.1.7
+     + colorama==0.4.6
      + flask==3.0.0
      + itsdangerous==2.1.2
      + jinja2==3.1.2
