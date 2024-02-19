a2f51b6575726f6ed91f2aba6dee52a2be50c654
j178 <10510431+j178@users.noreply.github.com>
2024-02-19 17:10:43 +0800

    `rye tools list` show broken tools

--- rye/src/cli/tools.rs
+++ rye/src/cli/tools.rs
@@ -43,8 +43,12 @@ fn list_tools(cmd: ListCommand) -> Result<(), Error> {
     tools.sort();
 
     for (tool, mut info) in tools {
+        if !info.valid {
+            echo!("{} ({})", style(tool).red(), style("seems broken").red());
+            continue;
+        }
         if cmd.version_show {
-            echo!("{} {}", style(tool).cyan(), style(info.version).cyan());
+            echo!("{} ({})", style(tool).cyan(), info.version);
         } else {
             echo!("{}", style(tool).cyan());
         }
