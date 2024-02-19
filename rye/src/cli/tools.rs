astral-sh/rye

commit a2790fd674a0434d6885fa8d0cb27836939f27de

`rye tools list` shows broken tools (#692)

rye/src/cli/tools.rs
@@ -43,8 +43,12 @@ fn list_tools(cmd: ListCommand) -> Result<(), Error> {
     tools.sort();
 
     for (tool, mut info) in tools {
-        if cmd.version_show {
+        if !info.valid {
             echo!("{} {}", style(tool).cyan(), style(info.version).cyan());
+            continue;
+        }
+        if cmd.version_show {
+            echo!("{} ({})", style(tool).cyan(), info.version);
         } else {
             echo!("{}", style(tool).cyan());
         }

rye/src/installer.rs
@@ -72,11 +72,16 @@ static SUCCESSFULLY_DOWNLOADED_RE: Lazy<Regex> =
 pub struct ToolInfo {
     pub version: String,
     pub scripts: Vec<String>,
+    pub valid: bool,
 }
 
 impl ToolInfo {
-    pub fn new(version: String, scripts: Vec<String>) -> Self {
-        Self { version, scripts }
+    pub fn new(version: String, scripts: Vec<String>, valid: bool) -> Self {
+        Self {
+            version,
+            scripts,
+            valid,
+        }
     }
 }
 
@@ -347,17 +352,20 @@ pub fn list_installed_tools() -> Result<HashMap<String, ToolInfo>, Error> {
                 }
             }
         }
-        let tool_version = Command::new(target_venv_bin_path.join("python"))
+
+        let output = Command::new(target_venv_bin_path.join("python"))
             .arg("-c")
             .arg(TOOL_VERSION_SCRIPT)
             .arg(tool_name.clone())
             .stdout(Stdio::piped())
-            .output()?;
-        let tool_version = String::from_utf8_lossy(&tool_version.stdout)
-            .trim()
-            .to_string();
+            .output();
+        let valid = output.is_ok();
+        let tool_version = match output {
+            Ok(output) => String::from_utf8_lossy(&output.stdout).trim().to_string(),
+            Err(_) => String::new(),
+        };
 
-        rv.insert(tool_name, ToolInfo::new(tool_version, scripts));
+        rv.insert(tool_name, ToolInfo::new(tool_version, scripts, valid));
     }
 
     Ok(rv)

commit 2f8868e08069d133863be00315459204e9537122

Fix `rye tools list` format issue introduced in #692
The last commit wrongly changed the format, restore it.

rye/src/cli/tools.rs
@@ -44,11 +44,11 @@ fn list_tools(cmd: ListCommand) -> Result<(), Error> {
 
     for (tool, mut info) in tools {
         if !info.valid {
-            echo!("{} {}", style(tool).cyan(), style(info.version).cyan());
+            echo!("{} ({})", style(tool).red(), style("seems broken").red());
             continue;
         }
         if cmd.version_show {
-            echo!("{} ({})", style(tool).cyan(), info.version);
+            echo!("{} {}", style(tool).cyan(), style(info.version).cyan());
         } else {
             echo!("{}", style(tool).cyan());
         }