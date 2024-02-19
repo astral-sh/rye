e36e1ffaa46131b4e5b4d2b0871ce36d68d2e75a
j178 <10510431+j178@users.noreply.github.com>
2024-02-19 11:32:10 +0800

    Improve fetch

--- rye-devtools/src/rye_devtools/find_downloads.py
+++ rye-devtools/src/rye_devtools/find_downloads.py
@@ -193,32 +193,34 @@ class IndygregDownload:
         return None
 
 
-async def fetch(client: httpx.AsyncClient, page: str, headers: dict[str, str]):
-    """Fetch a page from GitHub API with ratelimit awareness."""
-    resp = await client.get(page, headers=headers, timeout=90)
+async def fetch(client: httpx.AsyncClient, url: str) -> httpx.Response:
+    """Fetch from GitHub API with rate limit awareness."""
+    resp = await client.get(url, timeout=15)
     if (
         resp.status_code in [403, 429]
         and resp.headers.get("x-ratelimit-remaining") == "0"
     ):
         # See https://docs.github.com/en/rest/using-the-rest-api/troubleshooting-the-rest-api?apiVersion=2022-11-28
         if (retry_after := resp.headers.get("retry-after")) is not None:
-            log("got retry-after header. retrying in {retry_after} seconds.")
+            log(f"Got retry-after header, retry in {retry_after} seconds.")
             time.sleep(int(retry_after))
 
-            return await fetch(client, page, headers)
+            return await fetch(client, url)
 
         if (retry_at := resp.headers.get("x-ratelimit-reset")) is not None:
             utc = datetime.now(timezone.utc).timestamp()
-            retry_after = int(retry_at) - int(utc)
+            retry_after = max(int(retry_at) - int(utc), 0)
 
-            log("got x-ratelimit-reset header. retrying in {retry_after} seconds.")
-            time.sleep(max(int(retry_at) - int(utc), 0))
+            log(f"Got x-ratelimit-reset header, retry in {retry_after} seconds.")
+            time.sleep(retry_after)
 
-            return await fetch(client, page, headers)
+            return await fetch(client, url)
 
-        log("got rate limited but no information how long. waiting for 2 minutes")
+        log("Got rate limited but no information how long, wait for 2 minutes.")
         time.sleep(60 * 2)
-        return await fetch(client, page, headers)
+        return await fetch(client, url)
+
+    resp.raise_for_status()
     return resp
 
 
