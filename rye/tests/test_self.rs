071ddae350c149dfdddb23e73a092c0821a354fc
j178 <10510431+j178@users.noreply.github.com>
2024-02-20 20:55:40 +0800

    Remove `tools` directory during self uninstallation
    Also included a test, however, this test is self-destructive and might interfere with other tests, not sure it is the right thing to test.

---
+++ rye/tests/test_self.rs
@@ -0,0 +1,45 @@
+use std::process::Stdio;
+use crate::common::Space;
+mod common;
+
+#[test]
+fn test_self_uninstall() {
+    let space = Space::new();
+    let _guard = space.lock_rye_home();
+
+    // install a global tool to ensure tools directory is created
+    space
+        .rye_cmd()
+        .arg("install")
+        .arg("pycowsay")
+        .arg("-f")
+        .status()
+        .unwrap();
+
+    assert!(space.rye_home().join("self").is_dir());
+    assert!(space.rye_home().join("py").is_dir());
+    assert!(space.rye_home().join("tools").is_dir());
+
+    let status = space
+        .rye_cmd()
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .arg("self")
+        .arg("uninstall")
+        .arg("--yes")
+        .status()
+        .unwrap();
+    assert!(status.success());
+
+    let may_left = &["env", "config.toml", "lock"];
+    let leftovers: Vec<_> = space
+        .rye_home()
+        .read_dir()
+        .unwrap()
+        .filter(|x| {
+            let x = x.as_ref().unwrap();
+            !may_left.contains(&x.file_name().to_str().unwrap())
+        })
+        .collect();
+    assert!(leftovers.is_empty(), "leftovers: {:?}", leftovers);
+}
