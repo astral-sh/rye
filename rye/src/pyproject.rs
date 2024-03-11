71c827a247e366cc412fa554941fd323241ad2b5
David Soria Parra <experimentalworks@gmail.com>
2024-03-11 12:02:17 +0000

    pyproject: method to create empty ExpandedSourecs
    We are creating an empty ExpandedSources, as we will use them later to
    implement a default struct of Uv.

--- rye/src/pyproject.rs
+++ rye/src/pyproject.rs
@@ -1283,6 +1283,15 @@ pub struct ExpandedSources {
 }
 
 impl ExpandedSources {
+    #[allow(unused)]
+    pub fn empty() -> ExpandedSources {
+        ExpandedSources {
+            index_urls: Vec::new(),
+            find_links: Vec::new(),
+            trusted_hosts: HashSet::new(),
+        }
+    }
+
     /// Takes some sources and expands them.
     pub fn from_sources(sources: &[SourceRef]) -> Result<ExpandedSources, Error> {
         let mut index_urls = Vec::new();
