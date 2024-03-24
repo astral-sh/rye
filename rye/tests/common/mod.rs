0bac64bbe4df628ef617b160801ef2ec091e277f
bluss <bluss@users.noreply.github.com>
2024-03-24 12:37:00 +0100

    tests: Test for requirements.txt import

--- rye/tests/common/mod.rs
+++ rye/tests/common/mod.rs
@@ -255,3 +255,14 @@ macro_rules! rye_cmd_snapshot {
 
 #[allow(unused_imports)]
 pub(crate) use rye_cmd_snapshot;
+
+#[allow(unused)]
+pub fn toml_array_as_string_array(arr: &toml_edit::Array) -> Vec<String> {
+    arr.iter()
+        .map(|x| {
+            x.as_str()
+                .map(|x| x.to_string())
+                .unwrap_or_else(|| x.to_string())
+        })
+        .collect()
+}
